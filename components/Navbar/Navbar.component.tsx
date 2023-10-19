43b36c871ff2c6003f4ef212c6f5d42069eaa67a
Alejo <alejoivanrojas@gmail.com>
2023-10-18 23:49:20 -0300

    add responsive navbar design

--- components/Navbar/Navbar.component.tsx
+++ components/Navbar/Navbar.component.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='p-12 w-full flex justify-between text-center items-center '>
-        <div className='xs:hidden md:block lg:block'>
+    <nav className='p-12 xs:p-6 xs:pt-8 xs:pr-5 w-full flex justify-between text-center items-center '>
+        <div className='xs:hidden md:hidden lg:block'>
           <Image src="/logo.svg" width={200} height={200} alt='logo-basement' />
         </div>
-        <div className='md:hidden lg:hidden'>
+        <div className='lg:hidden'>
           <Image src="/logo-sm.png" width={50} height={50} alt='logo-basement' />
         </div>
-        <div className='flex'>
-          <Image src="/navbar.svg" className='w-72' width={100} height={200} alt='icon-basement' />
+        <div className='flex xs:hidden md:hidden lg:block'>
+          <Image src="/navbar.svg" className='w-72' priority={true} width={100} height={200} alt='icon-basement' />
         </div>
-        <div className='m-0 p-3 w-40 cursor-pointer border-[3px] rounded-[2rem] border-solid border-white'>
-          <button className=''>
+        <div className='m-0 p-3 w-40 xs:w-32 xs:border-2  cursor-pointer border-[3px] rounded-[2rem] border-solid border-white'>
+          <button className='cursor-pointer'>
               CART (0)
           </button>
         </div>
