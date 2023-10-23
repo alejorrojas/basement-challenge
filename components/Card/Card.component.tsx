24134dcd0d590a5c527fc15fb8a14312c23a7d20
Alejo <alejoivanrojas@gmail.com>
2023-10-22 21:45:17 -0300

    add shadow card

--- components/Card/Card.component.tsx
+++ components/Card/Card.component.tsx
@@ -4,15 +4,15 @@ import { Product } from '../../data/products.types'
 
 const Card: FC<Product> = ({image, name, price}) => {
   return (
-    <div className=' flex justify-center items-center gap-4 flex-col'>
-      <Image className='md:max-lg:max-h-xl md:max-lg:max-w-xl' height={1000} width={1000} src={image} alt={name} />
+      <div className=' flex justify-center items-center gap-4 flex-col'>
+        <Image className='bg-gradient-to-t from-[#1c1c1c] hover:from-black  md:max-lg:max-h-xl md:max-lg:max-w-xl' height={1000} width={1000} src={image} alt={name} />
+      
+        <div className='w-full xl:text-3xl md:max-lg:max-w-xl  lg:text-xl flex border-t-4 pt-4 border-white justify-between'>
+          <p>{name}</p>
+          <p>$ {price}</p>
+        </div>
 
-      <div className='w-full xl:text-3xl md:max-lg:max-w-xl lg:text-xl flex border-t-4 pt-4 border-white justify-between'>
-        <p>{name}</p>
-        <p>$ {price}</p>
       </div>
-
-    </div>
   )
 }
 
