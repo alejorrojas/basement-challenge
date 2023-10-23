6bb6e255501e6d821d1a844416180dcffb9dda21
Alejo <alejoivanrojas@gmail.com>
2023-10-23 00:18:28 -0300

    fix hover effect on mobile

--- components/Card/Card.component.tsx
+++ components/Card/Card.component.tsx
@@ -7,7 +7,7 @@ const Card: FC<Product> = ({image, name, price}) => {
   
         <div className='relative flex justify-center items-center flex-col group'>
           <Image className='bg-gradient-to-t from-[#1c1c1c] group-hover:from-black  ' height={1000} width={1000} src={image} alt={name} />
-          <div className='absolute w-full h-full -top-10 flex justify-center invisible group-hover:visible transition-all ease-in-out delay-200'>
+          <div className='xs:hidden lg:flex absolute w-full h-full -top-10  justify-center invisible group-hover:visible transition-all ease-in-out delay-200'>
               <Image className='xs:w-48 md:w-72 lg:w-32 xl:w-56 2xl:w-72'  src='/hover.svg' width={300} height={300} alt='' />
           </div>
           <div className='w-full xl:text-3xl  lg:text-xl flex border-t-4 pt-4 border-white justify-between'>
