466c5d70ec1bc8435f43e4b6ff0f58cacef37a01
Alejo <alejoivanrojas@gmail.com>
2023-10-25 14:05:11 -0300

    add responsive desktop styles

--- components/Card/CardCheckout.component.tsx
+++ components/Card/CardCheckout.component.tsx
@@ -7,14 +7,16 @@ const CardCheckout: FC<Product> = ({image, name, description, id, options, price
   const [products, setProducts] = useLocalStorage<ProductCheckout[]>('products', [])
 
   const productIndex = products.findIndex((product) => product.id === id);
-  const checkoutProduct = products[productIndex]
-  const updatedProducts = [...products];
+  //Target the specific product
+  const checkoutProduct = products[productIndex] 
+  //Make a copy of the products array
+  const updatedProducts = [...products] 
 
   const addQuantity = () =>{
     const newQuantity = checkoutProduct?.quantity + 1
 
-    updatedProducts[productIndex].quantity = newQuantity
-
+    //Modify just the selected element
+    updatedProducts[productIndex].quantity = newQuantity 
     setProducts(updatedProducts)
 
   }
@@ -28,48 +30,50 @@ const CardCheckout: FC<Product> = ({image, name, description, id, options, price
        setProducts(filterProducts)
     }
     else {
-      updatedProducts[productIndex].quantity = newQuantity
+      //Modify just the selected element
+      updatedProducts[productIndex].quantity = newQuantity  
       setProducts(updatedProducts)
     }
   }
   
   const updateSize = (newSize: string)=>{
-    updatedProducts[productIndex].checkoutSize = newSize
+    //Modify just the selected element
+    updatedProducts[productIndex].checkoutSize = newSize 
     setProducts(updatedProducts)
   }
 
   return (
-    <div className='flex border-solid border-2 border-white w-full p-4 gap-10'>
-      <Image className='bg-gradient-to-t from-[#1c1c1c]' width={200} height={300} src={image} alt={name} />
+    <div className='flex border-solid border-2 border-white w-full p-4 gap-3 2xl:gap-10'>
+      <Image className='bg-gradient-to-t from-[#1c1c1c]  lg:max-2xl:w-36' width={200} height={300} src={image} alt={name} />
 
       <div className='flex flex-col justify-between w-full'>
         <header>
-          <h3 className='text-3xl'>{name.toUpperCase()}</h3>
-          <h4 className='text-xl text-[#999999] '>{description}</h4>
+          <h3 className='text-[1rem] 2xl:text-3xl'>{name.toUpperCase()}</h3>
+          <h4 className='text-base 2xl:text-xl text-[#999999] '>{description}</h4>
         </header>
 
         <footer className='relative flex justify-between'>
             <div className='flex flex-col gap-4'>
               <div className='flex gap-8 items-center'>
-                <h4 className='text-2xl'>QUANTITY: </h4>
-                <div className='m-0 xs:p-1 md:p-2 md:px-3  w-24 flex justify-between text-lg xs:border-2 border-[3px] rounded-[2rem] border-solid border-white cursor-pointer'>
+                <h4 className='text-[1rem] 2xl:text-2xl'>QUANTITY: </h4>
+                <div className='m-0 p-1 2xl:p-2 xl:px-3  w-24 flex justify-between text-lg xs:border-2 border-[3px] rounded-[2rem] border-solid border-white cursor-pointer'>
                   <button onClick={removeQuantity} >-</button>
-                  <span className='text-xl' >{checkoutProduct?.quantity}</span>
+                  <span className='text-[1rem] 2xl:text-xl' >{checkoutProduct?.quantity}</span>
                   <button onClick={addQuantity} >+</button>
                 </div>
               </div>
 
-              <div className='flex gap-6 items-center'>
-                <h4 className='text-2xl' >SIZE: </h4>
+              <div className='flex gap-2 2xl:gap-6 items-center'>
+                <h4 className='text-[1rem] 2xl:text-2xl' >SIZE: </h4>
 
                 {options[0].values.map(value =>(
-                  <button  onClick={()=>updateSize(value)} className={`${value === checkoutProduct.checkoutSize ? "border-white border-2 rounded-full  px-3 py-2 " : "border-black border-2 rounded-full  px-3 py-2 "} `} key={value}>
+                  <button  onClick={()=>updateSize(value)} className={`${value === checkoutProduct.checkoutSize ? "border-white border-2 rounded-full " : "border-black border-2 rounded-full "} xl:px-2 xl:py-1 2xl:px-3 2xl:py-2  text-base `} key={value}>
                     {value}
                   </button>
                 )) }
               </div>
             </div>
-            <h3 className='text-4xl absolute right-0 bottom-0'>${price}</h3>
+            <h3 className='text-2xl 2xl:text-4xl absolute right-0 bottom-0'>${price}</h3>
           </footer>
        </div>
     </div>
--- components/Navbar/ModalCheckout.component.tsx
+++ components/Navbar/ModalCheckout.component.tsx
@@ -14,20 +14,20 @@ const ModalCheckout = () => {
 
   return (
     <Dialog.Root>
-        <Dialog.Trigger  className='m-0 xs:p-2 md:p-3 w-40 xs:w-32 2xl:w-40 xs:border-2 border-[3px] rounded-[2rem] border-solid border-white cursor-pointer 2xl:text-xl'>
+        <Dialog.Trigger className='m-0 xs:p-2 md:p-3 w-40 xs:w-32 2xl:w-40 xs:border-2 border-[3px] rounded-[2rem] border-solid border-white cursor-pointer 2xl:text-xl'>
             CART ({products.length})
         </Dialog.Trigger>
         <Dialog.Portal>
             <Dialog.Overlay className='fixed inset-0 bg-black/50' />
-            <Dialog.Content className='flex justify-between flex-col z-50 fixed top-0 right-0 w-[45%] xs:max-md:w-screen xs:max-md:h-screen h-[90%] bg-black border-solid border-t-0 border-r-0 border-white border-2'>
+            <Dialog.Content className='flex  justify-between flex-col z-50 fixed top-0 right-0 xl:w-[45%] min-h-[90%] lg:w-[60%] xs:max-lg:w-screen xs:max-lg:h-screen  bg-black border-solid border-t-0 border-r-0 border-white border-2'>
             <div className='p-8 flex flex-col'>
                 <Dialog.Close className='cursor-pointer text-right text-xl mb-10 font-extrabold' >→ CLOSE</Dialog.Close>
-                <Dialog.Title className='xs:max-md:flex-col mb-8 justify-between flex text-center text-9xl'>
+                <Dialog.Title className='xs:max-lg:flex-col md:mb-4 2xl:mb-8 justify-center gap-[3vw] flex text-center xs:text-7xl xl:text-[5rem] 2xl:text-8xl'>
                     <span>YOUR</span> 
                     <span className='text-black text-shadow-white' >CART</span>
                 </Dialog.Title>
             
-                <div className='overflow-y-auto max-h-96 flex flex-col gap-10'>
+                <div className='overflow-y-auto xs:max-h-[60vh] xl:max-h-[50vh]  max-h-96 flex flex-col gap-10'>
                     <div className='px-2 flex gap-8 flex-col'>
                         {products.map(product => (
                             <CardCheckout key={product.id}  {...product} />     
@@ -35,7 +35,7 @@ const ModalCheckout = () => {
                     </div>
                 </div>
                 </div>
-                <div className='flex justify-between text-4xl border-solid border-t-2 border-white'>
+                <div className='flex justify-between xs:text-2xl xl:text-2xl 2xl:text-4xl border-solid border-t-2 border-white'>
                     <h3 className='p-8 w-3/4'>TOTAL: ${totalPrice()} </h3>
                     <div className="border-l-2"/>
                     <h3 className='p-8 text-black text-shadow-white' > CHECKOUT</h3>
