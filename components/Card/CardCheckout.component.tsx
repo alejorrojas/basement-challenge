8ddf815626cc52324926fc576a8d96cb355d4a7e
Alejo <alejoivanrojas@gmail.com>
2023-10-25 18:39:42 -0300

    fix btn padding

--- components/Card/CardCheckout.component.tsx
+++ components/Card/CardCheckout.component.tsx
@@ -56,7 +56,7 @@ const CardCheckout: FC<Product> = ({image, name, description, id, options, price
             <div className='flex flex-col gap-4'>
               <div className='flex xs:gap-4 gap-8 items-center'>
                 <h4 className='xs:max-md:text-xs  text-[1rem] 2xl:text-2xl'>QUANTITY: </h4>
-                <div className='m-0 xs:w-16 xs:px-2 p-1 2xl:p-2 xl:px-3  md:w-24 flex justify-between xs:max-md:text-xs  md:text-lg xs:border-2 border-[3px] rounded-[2rem] border-solid border-white cursor-pointer'>
+                <div className='m-0 xs:w-16 xs:px-2  2xl:py-2  xl:px-5  md:w-24 flex justify-between xs:max-md:text-xs  md:text-lg xs:border-2 border-[3px] rounded-[2rem] border-solid border-white cursor-pointer'>
                   <button onClick={removeQuantity} >-</button>
                   <span className='xs:text-xs text-[1rem] 2xl:text-xl' >{checkoutProduct?.quantity}</span>
                   <button onClick={addQuantity} >+</button>
@@ -64,10 +64,10 @@ const CardCheckout: FC<Product> = ({image, name, description, id, options, price
               </div>
 
               <div className='flex gap-1 2xl:gap-6 items-center'>
-                <div className='flex items-center'>
+                <div className='flex items-center gap-3'>
                   <h4 className='xs:max-md:text-xs text-[1rem] 2xl:text-2xl' >SIZE: </h4>
                   {options[0].values.map(value =>(
-                    <button  onClick={()=>updateSize(value)} className={`${value === checkoutProduct.checkoutSize ? "border-white border-2  rounded-full " : "border-black border-2   rounded-full "} xs:max-md:border-[1px] xs:px-1 xl:px-3 xl:py-1 2xl:px-3 2xl:py-2 xs:max-md:text-sm  text-base `} key={value}>
+                    <button  onClick={()=>updateSize(value)} className={`${value === checkoutProduct.checkoutSize ? "border-white border-2  rounded-full " : "border-black border-2   rounded-full "} xs:max-md:border-[1px] xs:px-1 xl:px-3 xl:py-1 2xl:px-3 2xl:py-1 xs:max-md:text-sm  text-base`} key={value}>
                       {value}
                     </button>
                   )) }
